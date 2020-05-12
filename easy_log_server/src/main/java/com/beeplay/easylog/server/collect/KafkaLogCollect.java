c61864a224783417b3e56dc86d26608d65cf16bb
fayechenlong <409111252@qq.com>
2020-05-12 14:19:56 +0800

    * 优化代码

--- easy_log_server/src/main/java/com/beeplay/easylog/server/collect/KafkaLogCollect.java
+++ easy_log_server/src/main/java/com/beeplay/easylog/server/collect/KafkaLogCollect.java
@@ -5,22 +5,33 @@ import com.beeplay.easylog.server.InitConfig;
 import com.beeplay.easylog.server.es.ElasticSearchClient;
 import com.beeplay.easylog.server.util.DateUtil;
 import com.beeplay.easylog.server.util.GfJsonUtil;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.slf4j.LoggerFactory;
-
 import java.time.Duration;
 import java.util.*;
 import java.util.concurrent.CopyOnWriteArrayList;
-
+/**
+* @Author Frank.chen
+* @Description //TODO
+* @Date 14:15 2020/5/12
+* @Param 
+* @return 
+**/
 public class KafkaLogCollect {
 
-    private static org.slf4j.Logger logger= LoggerFactory.getLogger(KafkaLogCollect.class);
-    private static List<Map<String,Object>> list=new CopyOnWriteArrayList();
+    private  org.slf4j.Logger logger= LoggerFactory.getLogger(KafkaLogCollect.class);
 
+    private  List<Map<String,Object>> list=new CopyOnWriteArrayList();
 
-    public static void kafkaStart(String kafkaHosts,String esHosts){
+    private String kafkaHosts;
+    private String esHosts;
+
+    public KafkaLogCollect(String kafkaHosts,String esHosts){
+        this.kafkaHosts=kafkaHosts;
+        this.esHosts=esHosts;
+    }
+    public  void kafkaStart(){
         KafkaConsumer<String, String> consumer = KafkaConsumerClient.getInstance(kafkaHosts).getKafkaConsumer();
         consumer.subscribe(Arrays.asList(InitConfig.LOG_KEY));
         logger.info("getting log ready!");
@@ -44,7 +55,7 @@ public class KafkaLogCollect {
             }
         }
     }
-    private static void sendLog(ElasticSearchClient ec){
+    private  void sendLog(ElasticSearchClient ec){
         try {
             ec.insertList(list,InitConfig.ES_INDEX+ DateUtil.parseDateToStr(new Date(),DateUtil.DATE_FORMAT_YYYYMMDD),InitConfig.ES_TYPE);
             logger.info("log insert es success! count:"+list.size());
--- easy_log_server/src/main/java/com/beeplay/easylog/server/collect/RedisLogCollect.java
+++ easy_log_server/src/main/java/com/beeplay/easylog/server/collect/RedisLogCollect.java
@@ -14,15 +14,29 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ThreadPoolExecutor;
-
+/**
+* @Author Frank.chen
+* @Description //TODO
+* @Date 14:15 2020/5/12
+* @Param 
+* @return 
+**/
 public class RedisLogCollect {
-    private static ThreadPoolExecutor threadPoolExecutor = ThreadPoolUtil.getPool();
-    private static org.slf4j.Logger logger= LoggerFactory.getLogger(RedisLogCollect.class);
-    private static List<Map<String,Object>> list=new CopyOnWriteArrayList();
+    private  org.slf4j.Logger logger= LoggerFactory.getLogger(RedisLogCollect.class);
+    private  ThreadPoolExecutor threadPoolExecutor = ThreadPoolUtil.getPool();
+    private  List<Map<String,Object>> list=new CopyOnWriteArrayList();
 
-    public static void redisStart(String redisHot,int redisPort,String esHosts){
+    private String redisHost;
+    private int redisPort;
+    private String esHosts;
 
-        RedisClient redisClient=RedisClient.getInstance(redisHot,redisPort,"");
+    public RedisLogCollect(String redisHot,int redisPort,String esHosts){
+        this.redisHost=redisHot;
+        this.redisPort=redisPort;
+        this.esHosts=esHosts;
+    }
+    public  void redisStart(){
+        RedisClient redisClient=RedisClient.getInstance(redisHost,redisPort,"");
         logger.info("getting log ready!");
         ElasticSearchClient ec=ElasticSearchClient.getInstance(esHosts);
         logger.info("sending log ready!");
@@ -35,19 +49,19 @@ public class RedisLogCollect {
 
 
     }
-    public static void collectRuningLog(RedisClient redisClient, ElasticSearchClient ec){
+    private  void collectRuningLog(RedisClient redisClient, ElasticSearchClient ec){
         while (true) {
             List<String> logs=redisClient.getMessage(InitConfig.LOG_KEY,InitConfig.MAX_SEND_SIZE);
             collect(logs,ec,InitConfig.ES_INDEX+ DateUtil.parseDateToStr(new Date(),DateUtil.DATE_FORMAT_YYYYMMDD));
         }
     }
-    public static void collectTraceLog(RedisClient redisClient, ElasticSearchClient ec){
+    private  void collectTraceLog(RedisClient redisClient, ElasticSearchClient ec){
         while (true) {
             List<String> logs=redisClient.getMessage(InitConfig.LOG_KEY+"_"+ LogTypeContext.LOG_TYPE_TRACE,InitConfig.MAX_SEND_SIZE);
             collect(logs,ec,InitConfig.ES_INDEX+LogTypeContext.LOG_TYPE_TRACE+"_"+ DateUtil.parseDateToStr(new Date(),DateUtil.DATE_FORMAT_YYYYMMDD));
         }
     }
-    private static void collect(List<String> logs,ElasticSearchClient ec,String index){
+    private  void collect(List<String> logs,ElasticSearchClient ec,String index){
         if(logs.size()>0) {
             logger.info("get log " + " " + list.size() + " counts!");
             logs.forEach(log->{
@@ -65,7 +79,7 @@ public class RedisLogCollect {
             e.printStackTrace();
         }
     }
-    private static void sendLog(ElasticSearchClient ec,String index){
+    private  void sendLog(ElasticSearchClient ec,String index){
         try {
             ec.insertList(list,index,InitConfig.ES_TYPE);
             logger.info("log insert es success! count:"+list.size());
