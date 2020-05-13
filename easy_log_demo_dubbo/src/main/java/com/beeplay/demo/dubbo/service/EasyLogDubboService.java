96c365788dd6fdc98627cb405fe43e1706a3afc4
fayechenlong <409111252@qq.com>
2020-05-13 16:11:11 +0800

    * 增加dubbo demo

---
+++ easy_log_demo_dubbo/src/main/java/com/beeplay/demo/dubbo/service/EasyLogDubboService.java
@@ -0,0 +1,12 @@
+package com.beeplay.demo.dubbo.service;
+
+/**
+ * @InterfaceName EasyLogDubboService
+ * @Deacription TODO
+ * @Author Frank.Chen
+ * @Date 2020/5/13 16:04
+ * @Version 1.0
+ **/
+public interface EasyLogDubboService {
+    void testLogDubbo();
+}
