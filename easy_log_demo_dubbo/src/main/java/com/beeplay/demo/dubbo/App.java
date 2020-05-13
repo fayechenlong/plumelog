458966ca8af7471c20f37ac724405ed846f80186
fayechenlong <409111252@qq.com>
2020-05-13 16:11:47 +0800

    * 增加dubbo demo

---
+++ easy_log_demo_dubbo/src/main/java/com/beeplay/demo/dubbo/App.java
@@ -0,0 +1,16 @@
+package com.beeplay.demo.dubbo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+* @Author Frank.chen
+* @Description //TODO
+* @Date 15:58 2020/5/13
+* @Param 
+* @return 
+**/
+@SpringBootApplication
+public class App {
+    public static void main( String[] args ){
+        SpringApplication.run(App.class, args);
+    }
+}
