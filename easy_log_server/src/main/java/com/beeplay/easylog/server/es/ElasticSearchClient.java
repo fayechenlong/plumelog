9f368af126ab98cdb13624d9e6ff078e3f663d97
fayechenlong <409111252@qq.com>
2020-05-18 18:17:52 +0800

    * 更新es版本

--- easy_log_server/src/main/java/com/beeplay/easylog/server/es/ElasticSearchClient.java
+++ easy_log_server/src/main/java/com/beeplay/easylog/server/es/ElasticSearchClient.java
@@ -1,12 +1,18 @@
 package com.beeplay.easylog.server.es;
 
+import com.beeplay.easylog.server.collect.RedisLogCollect;
 import org.apache.http.HttpHost;
+import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.delete.DeleteRequest;
 import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
@@ -19,6 +25,7 @@ import java.util.UUID;
 * @return 
 **/
 public class ElasticSearchClient {
+    private  org.slf4j.Logger logger= LoggerFactory.getLogger(ElasticSearchClient.class);
     private static ElasticSearchClient instance;
     private RestHighLevelClient client;
     public static ElasticSearchClient getInstance(String hosts) {
@@ -40,23 +47,28 @@ public class ElasticSearchClient {
         RestClientBuilder builder = RestClient.builder(httpHosts);
        client = new RestHighLevelClient(builder);
     }
-    public void insertOrUpdate(Map map,String baseIndex,String baseType) throws Exception {
-        IndexRequest request = new IndexRequest(baseIndex, baseType,  UUID.randomUUID().toString());
-        request.source(map);
-        client.index(request);
-    }
-    public void delete(String baseIndex) throws Exception {
-        DeleteRequest request = new DeleteRequest(baseIndex);
-        client.delete(request);
-    }
-    public void insertList(List<Map<String,Object>> list,String baseIndex,String baseType) throws IOException {
+
+    public void insertList(List<Map<String,Object>> list,String baseIndex,String type) throws IOException {
         BulkRequest bulkRequest = new BulkRequest();
         list.forEach(map->{
-            IndexRequest request = new IndexRequest(baseIndex, baseType, UUID.randomUUID().toString());
+            IndexRequest request = new IndexRequest(baseIndex);
             request.source(map);
             bulkRequest.add(request);
         });
-        client.bulk(bulkRequest);
+        client.bulkAsync(bulkRequest, RequestOptions.DEFAULT, new ActionListener<BulkResponse>() {
+            @Override
+            public void onResponse(BulkResponse bulkResponse) {
+
+                logger.info("ElasticSearch commit success!");
+                //成功
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+                //失败
+                logger.info("ElasticSearch commit Failure!");
+            }
+        });
     }
     public void close(){
         try {
