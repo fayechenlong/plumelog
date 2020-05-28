a7ef953e77aff8b1f6205b4a0454eb8f54340527
delicious28 <delicious28@gmail.com>
2020-05-28 14:42:21 +0800

    remove

--- easy_log_ui_front/src/views/Trace.vue
+++ easy_log_ui_front/src/views/Trace.vue
@@ -25,7 +25,6 @@
 
 <script>
 // @ is an alias to /src
-// import HelloWorld from "@/components/HelloWorld.vue";
 import axios from '@/services/http'
 import _ from 'lodash'
 import moment from 'moment'
