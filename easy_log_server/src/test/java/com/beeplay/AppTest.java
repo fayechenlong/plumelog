2129dc5ef51993d262d4c67f2844044341674ffb
fayechenlong <409111252@qq.com>
2020-05-29 16:13:06 +0800

    * 修改文档

--- easy_log_server/src/test/java/com/beeplay/AppTest.java
+++ easy_log_server/src/test/java/com/beeplay/AppTest.java
@@ -1,11 +1,7 @@
 package com.beeplay;
 
 
-import com.beeplay.easylog.core.util.IpGetter;
-import com.beeplay.easylog.core.util.IpUtils;
-import com.beeplay.easylog.server.es.ElasticLowerClient;
 import org.junit.Test;
-import java.util.List;
 
 /**
  * Unit test for simple App.
