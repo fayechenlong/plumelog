ce3d3bbd734f25d2f2ae5fd4fffcd7ed97edfa9f
fayechenlong <409111252@qq.com>
2020-05-29 10:41:46 +0800

    * server加密码

--- easy_log_server/src/test/java/com/beeplay/AppTest.java
+++ easy_log_server/src/test/java/com/beeplay/AppTest.java
@@ -1,11 +1,8 @@
 package com.beeplay;
 
 
-import com.beeplay.easylog.core.util.IpGetter;
-import com.beeplay.easylog.core.util.IpUtils;
-import com.beeplay.easylog.server.es.ElasticLowerClient;
+
 import org.junit.Test;
-import java.util.List;
 
 /**
  * Unit test for simple App.
@@ -18,11 +15,7 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue(){
 
-        try {
-            System.out.println(IpUtils.getLocalHostIP());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+
 
     }
 }
