d53656aa48f675c25cfec58b244b370e3af36661
tank <252720074@qq.com>
2020-06-05 17:42:55 +0800

    新增filter的测试用例

--- plumelog-demo/src/main/java/com/plumelog/demo/Interceptor.java
+++ plumelog-demo/src/main/java/com/plumelog/demo/Interceptor.java
@@ -1,6 +1,8 @@
 package com.plumelog.demo;
 import com.plumelog.core.TraceId;
 import com.plumelog.core.util.IdWorker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
@@ -12,8 +14,11 @@ import javax.servlet.http.HttpServletResponse;
 @Component
 public class Interceptor extends HandlerInterceptorAdapter{
 
+    Logger logger = LoggerFactory.getLogger(Interceptor.class);
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        /*logger.info("进：preHandle ++++++++++++++++++++"+TraceId.logTraceID.get());
         String traceId = request.getHeader("TraceId");
         if (StringUtils.isEmpty(traceId)){
             IdWorker worker = new IdWorker(1,1,1);
@@ -21,6 +26,7 @@ public class Interceptor extends HandlerInterceptorAdapter{
         }else{
             TraceId.logTraceID.set(traceId);
         }
+        logger.info("退：preHandle ++++++++++++++++++++"+TraceId.logTraceID.get());*/
         return true;
     }
 }
