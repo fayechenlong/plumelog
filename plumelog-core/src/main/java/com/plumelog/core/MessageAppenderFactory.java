deb953aa3cf28f719be32f5a821d3be133a09ac3
tank <252720074@qq.com>
2020-06-16 10:36:12 +0800

    注释调整

--- plumelog-core/src/main/java/com/plumelog/core/MessageAppenderFactory.java
+++ plumelog-core/src/main/java/com/plumelog/core/MessageAppenderFactory.java
@@ -24,15 +24,22 @@ import java.util.concurrent.TimeUnit;
  */
 public class MessageAppenderFactory {
 
-    private static Boolean logOutPut=true;
-    private static int exceptionCount=0;
-    //异常超过maxExceptionCount不在输送日志到队列
-    private final static int maxExceptionCount=10;
-    //当下游异常的时候，状态缓存时间
-    private final static Cache<String, Boolean>  cache = CacheBuilder.newBuilder()
+    private static Boolean logOutPut = true;
+    private static int exceptionCount = 0;
+    /**
+     * 异常超过maxExceptionCount不在输送日志到队列
+     */
+
+    private final static int maxExceptionCount = 10;
+    /**
+     * 当下游异常的时候，状态缓存时间
+     */
+    private final static Cache<String, Boolean> cache = CacheBuilder.newBuilder()
             .expireAfterWrite(30, TimeUnit.SECONDS).build();
 
-    //设置阻塞队列为5000，防止OOM
+    /**
+     * 设置阻塞队列为5000，防止OOM
+     */
     private static ThreadPoolExecutor threadPoolExecutor
             = ThreadPoolUtil.getPool(4, 8, 5000);
 
@@ -40,25 +47,27 @@ public class MessageAppenderFactory {
         LogMessageProducer producer = new LogMessageProducer(LogRingBuffer.ringBuffer);
         producer.send(baseLogMessage);
     }
+
     public static void push(BaseLogMessage baseLogMessage, AbstractClient client) {
         final String redisKey =
                 baseLogMessage instanceof RunLogMessage
                         ? LogMessageConstant.LOG_KEY
                         : LogMessageConstant.LOG_KEY_TRACE;
-            threadPoolExecutor.execute(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        client.pushMessage(redisKey, GfJsonUtil.toJSONString(baseLogMessage));
-                    } catch (LogQueueConnectException e) {
-                        e.printStackTrace();
-                    }
+        threadPoolExecutor.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    client.pushMessage(redisKey, GfJsonUtil.toJSONString(baseLogMessage));
+                } catch (LogQueueConnectException e) {
+                    e.printStackTrace();
                 }
-            });
+            }
+        });
     }
 
     /**
      * 带队列宕机保护的push方法
+     *
      * @param baseLogMessage
      * @param client
      * @param logOutPutKey
@@ -68,26 +77,26 @@ public class MessageAppenderFactory {
                 baseLogMessage instanceof RunLogMessage
                         ? LogMessageConstant.LOG_KEY
                         : LogMessageConstant.LOG_KEY_TRACE;
-            threadPoolExecutor.execute(new Runnable() {
-                @Override
-                public void run() {
-                    logOutPut=cache.getIfPresent(logOutPutKey);
-                    if(logOutPut==null||logOutPut) {
-                        try {
-                            client.pushMessage(redisKey, GfJsonUtil.toJSONString(baseLogMessage));
-                            //写入成功重置异常数量
-                            logOutPut = true;
-                            exceptionCount = 0;
-                            cache.put(logOutPutKey, true);
-                        } catch (LogQueueConnectException e) {
-                            exceptionCount++;
-                            if (exceptionCount > maxExceptionCount) {
-                                cache.put(logOutPutKey, false);
-                            }
+        threadPoolExecutor.execute(new Runnable() {
+            @Override
+            public void run() {
+                logOutPut = cache.getIfPresent(logOutPutKey);
+                if (logOutPut == null || logOutPut) {
+                    try {
+                        client.pushMessage(redisKey, GfJsonUtil.toJSONString(baseLogMessage));
+                        //写入成功重置异常数量
+                        logOutPut = true;
+                        exceptionCount = 0;
+                        cache.put(logOutPutKey, true);
+                    } catch (LogQueueConnectException e) {
+                        exceptionCount++;
+                        if (exceptionCount > maxExceptionCount) {
+                            cache.put(logOutPutKey, false);
                             e.printStackTrace();
                         }
                     }
                 }
-            });
+            }
+        });
     }
 }
