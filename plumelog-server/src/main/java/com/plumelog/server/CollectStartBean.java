b4a011932d92870423ce4e1c1fc08625435cbba4
fayechenlong <409111252@qq.com>
2020-07-16 14:24:06 +0800

    * 优化代码结构

--- plumelog-server/src/main/java/com/plumelog/server/CollectStartBean.java
+++ plumelog-server/src/main/java/com/plumelog/server/CollectStartBean.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
