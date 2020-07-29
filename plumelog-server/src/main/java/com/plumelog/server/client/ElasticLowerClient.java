6c9a90f759255ff0eeaa86986452e08de130a748
chenlongfei <40911252@qq.com>
2020-07-29 12:09:32 +0800

    * appname keyword

--- plumelog-server/src/main/java/com/plumelog/server/client/ElasticLowerClient.java
+++ plumelog-server/src/main/java/com/plumelog/server/client/ElasticLowerClient.java
@@ -159,7 +159,9 @@ public class ElasticLowerClient {
 
     private void creatFieldData(String baseIndex, String field, String type) {
 
-        String ent = "{\"properties\":{\"" + field + "\":{\"type\":\"text\",\"fielddata\":true}}} ";
+        //String ent = "{\"properties\":{\"" + field + "\":{\"type\":\"text\",\"fielddata\":true,\"fields\": {\"raw\": {\"type\": \"keyword\"}}}}} ";
+
+        String ent = "{\"properties\":{\"" + field + "\":{\"type\":\"keyword\"}}} ";
         String endpoint = "";
         if (StringUtils.isEmpty(type)) {
             endpoint = "/" + baseIndex + "/_mapping";
