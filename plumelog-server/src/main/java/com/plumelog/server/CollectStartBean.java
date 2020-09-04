2afac571c301a3d573aab44df0dd0d6c3b2a651a
chenlongfei <40911252@qq.com>
2020-09-04 17:59:45 +0800

    * 增加按照小时创建索引的机制

--- plumelog-server/src/main/java/com/plumelog/server/CollectStartBean.java
+++ plumelog-server/src/main/java/com/plumelog/server/CollectStartBean.java
@@ -1,5 +1,6 @@
 package com.plumelog.server;
 
+import com.plumelog.core.constant.LogMessageConstant;
 import com.plumelog.core.redis.RedisClient;
 import com.plumelog.server.client.ElasticLowerClient;
 import com.plumelog.server.collect.KafkaLogCollect;
@@ -12,6 +13,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * className：CollectStartBean
@@ -50,11 +54,41 @@ public class CollectStartBean implements InitializingBean {
         }
     }
 
+    private String getRunLogIndex(Date date){
+        return LogMessageConstant.ES_INDEX + LogMessageConstant.LOG_TYPE_RUN + "_" + com.plumelog.server.util.DateUtil.parseDateToStr(date, com.plumelog.server.util.DateUtil.DATE_FORMAT_YYYYMMDD);
+    }
+    private String getTraceLogIndex(Date date){
+        return LogMessageConstant.ES_INDEX + LogMessageConstant.LOG_TYPE_TRACE + "_" + com.plumelog.server.util.DateUtil.parseDateToStr(date, com.plumelog.server.util.DateUtil.DATE_FORMAT_YYYYMMDD);
+    }
+    private String getRunLogIndex(Date date,String hour){
+        return LogMessageConstant.ES_INDEX + LogMessageConstant.LOG_TYPE_RUN + "_" + com.plumelog.server.util.DateUtil.parseDateToStr(date, com.plumelog.server.util.DateUtil.DATE_FORMAT_YYYYMMDD)+hour;
+    }
+    private String getTraceLogIndex(Date date,String hour){
+        return LogMessageConstant.ES_INDEX + LogMessageConstant.LOG_TYPE_TRACE + "_" + com.plumelog.server.util.DateUtil.parseDateToStr(date, com.plumelog.server.util.DateUtil.DATE_FORMAT_YYYYMMDD)+hour;
+    }
+
     @Override
     public void afterPropertiesSet() throws Exception {
 
         try {
             serverStart();
+
+
+            Calendar cal = Calendar.getInstance();
+            cal.add(Calendar.DATE, 1);
+            Date date = cal.getTime();
+            if(InitConfig.ES_INDEX_MODEL.equals("day")) {
+                creatIndiceLog(getRunLogIndex(date));
+                creatIndiceTrace(getRunLogIndex(date));
+            }else {
+                for (int a = 0; a < 24; a++) {
+                    String hour=String.format("%02d",a);
+                    creatIndiceLog(getRunLogIndex(date,hour));
+                    creatIndiceTrace(getRunLogIndex(date,hour));
+
+                }
+            }
+
         } catch (Exception e) {
             logger.error("plumelog server starting failed!", e);
         }
