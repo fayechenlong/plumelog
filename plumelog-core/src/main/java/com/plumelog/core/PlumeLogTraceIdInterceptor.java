a1974adeb398c7eabd0c6986e7f243907f6f188f
chenlongfei <409111252@qq.com>
2021-11-12 17:43:19 +0800

    * 优化代码

--- plumelog-core/src/main/java/com/plumelog/core/PlumeLogTraceIdInterceptor.java
+++ plumelog-core/src/main/java/com/plumelog/core/PlumeLogTraceIdInterceptor.java
@@ -1,6 +1,5 @@
 package com.plumelog.core;
 
-import com.plumelog.core.util.IdWorker;
 import com.plumelog.core.util.StringUtils;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 import javax.servlet.http.HttpServletRequest;
@@ -8,14 +7,11 @@ import javax.servlet.http.HttpServletResponse;
 
 
 public class PlumeLogTraceIdInterceptor extends HandlerInterceptorAdapter{
-
-    IdWorker worker = new IdWorker(1,1,1);
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String traceId = request.getHeader("traceId");
         if (StringUtils.isEmpty(traceId)){
-            IdWorker worker = new IdWorker(1,1,1);
-            TraceId.logTraceID.set(String.valueOf(worker.nextId()));
+            TraceId.set();
         }else{
             TraceId.logTraceID.set(traceId);
         }
